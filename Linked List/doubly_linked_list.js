49160c2ac31edd2ef1e49ab0708617e1dd15cfdb
Edward Ho <pythoned77@gmail.com>
2022-09-27 18:14:02 +0800

    set node class for Dlink

---
+++ Linked List/doubly_linked_list.js
@@ -0,0 +1,13 @@
+class Node {
+  constructor(element) {
+    this.element = element;
+    this.next = next;
+    this.previous = null;
+  }
+}
+
+class DList {
+  constructor() {
+    this.head = new Node("head");
+  }
+}
