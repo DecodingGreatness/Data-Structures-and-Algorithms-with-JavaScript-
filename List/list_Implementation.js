a8516f1bb08066fa4b2e46297856f50485ac1052
Edward Ho <pythoned77@gmail.com>
2022-09-27 16:25:26 +0800

    LList folder added

--- List/list_Implementation.js
+++ List/list_Implementation.js
@@ -3,22 +3,6 @@ class List {
     this.listSize = 0;
     this.pos = 0;
     this.dataStore = [];
-    this.clear = clear;
-    this.find = find;
-    this.toString = toString;
-    this.insert = insert;
-    this.append = append;
-    this.remove = remove;
-    this.front = front;
-    this.end = end;
-    this.prev = prev;
-    this.next = next;
-    this.length = length;
-    this.currPos = currPos;
-    this.moveTo = moveTo;
-    this.getElement = getElement;
-    this.length = length;
-    this.contains = contains;
   }
 
   append = (element) => {
@@ -33,4 +17,132 @@ class List {
     }
     return -1;
   };
+
+  remove = (element) => {
+    let foundAt = this.find(element);
+    if (foundAt > -1) {
+      this.dataStore.splice(foundAt, 1);
+      this.listSize--;
+      return true;
+    }
+    return false;
+  };
+
+  length = () => {
+    return this.listSize;
+  };
+
+  toSting = () => {
+    return this.dataStore;
+  };
+
+  insert = (element, after) => {
+    let insertPos = this.find(after);
+    if (insertPos > -1) {
+      this.dataStore.splice(insertPos + 1, 0, element);
+      this.listSize++;
+      return true;
+    }
+    return false;
+  };
+
+  clear = () => {
+    delete this.dataStore;
+    this.dataStore = [];
+    this.listSize = this.pos = 0;
+  };
+
+  contains = (element) => {
+    for (let i = 0; i < this.dataStore.length; i++) {
+      if ((this.dataStore[i] = element)) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  front = () => {
+    this.pos = 0;
+  };
+
+  end = () => {
+    this.pos = this.listSize - 1;
+  };
+
+  prev = () => {
+    if (this.pos > 0) {
+      this.pos--;
+    }
+  };
+
+  next = () => {
+    if (this.pos < this.listSize - 1) {
+      this.pos++;
+    }
+  };
+
+  currPos = () => {
+    return this.pos;
+  };
+
+  moveTo = (position) => {
+    this.pos = position;
+  };
+
+  getElement = () => {
+    return this.dataStore[this.pos];
+  };
 }
+
+// movie list
+function createArr(file) {
+  const { readFileSync } = require("fs");
+  let arr = readFileSync(file).toString().replace(/\r\n/g, "\n").split("\n");
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].trim();
+    console.log(arr[i]);
+  }
+}
+
+let displayList = (list) => {
+  for (list.front(); list.currPos() < list.length(); list.next()) {
+    if (list.getElement() instanceof Customer) {
+      console.log(list.getElement()["name"] + "," + list.getElement()["movie"]);
+    } else {
+      console.log(list.getElement());
+    }
+  }
+};
+
+// customer
+
+class Customer {
+  constructor(name, movie) {
+    this.name = name;
+    this.movie = movie;
+  }
+
+  checkOut = (name, movie, filmList, customerList) => {
+    if (movieList.contains(movie)) {
+      let c = new Customer(name, movie);
+      customerList.append(c);
+      filmList.remove(movie);
+    } else {
+      console.log(movie + "is not available.");
+    }
+  };
+}
+
+// test code
+let movies = createArr("films.txt");
+console.log(typeof movies);
+let movieList = new List();
+let customers = new List();
+for (let i = 0; i < movies.length; i++) {
+  movieList.append(movies[i]);
+}
+console.log("Available movies: \n");
+displayList(movieList);
+checkOut("Jane Doe", "The Godfather", movieList, customers);
+console.log("\nCustomer Rentals:\n");
+displayList(customers);
