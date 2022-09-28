84112bf966fd775123ba619c8ac3dd88da7cf955
Edward Ho <pythoned77@gmail.com>
2022-09-28 13:18:32 +0800

    added all methods to Dlist class

--- Linked List/doubly_linked_list.js
+++ Linked List/doubly_linked_list.js
@@ -1,7 +1,7 @@
 class Node {
   constructor(element) {
     this.element = element;
-    this.next = next;
+    this.next = null;
     this.previous = null;
   }
 }
@@ -10,4 +10,67 @@ class DList {
   constructor() {
     this.head = new Node("head");
   }
+
+  find(item) {
+    let currNode = this.head;
+    while (currNode.element != item) {
+      currNode = currNode.next;
+    }
+    return currNode;
+  }
+
+  insert(newElement, item) {
+    let newNode = new Node(newElement);
+    let current = this.find(item);
+    newNode.next = current.next;
+    newNode.previous = current;
+    current.next = newNode;
+  }
+
+  remove(item) {
+    let currNode = this.find(item);
+    if (!(currNode == null)) {
+      currNode.previous.next = currNode.next;
+      currNode.next.previous = currNode.previous;
+      currNode.next = null;
+      currNode.previous = null;
+    }
+  }
+
+  display() {
+    let currNode = this.head;
+    while (!(currNode.next == null)) {
+      console.log(currNode.next.element);
+      currNode = currNode.next;
+    }
+  }
+
+  findLast() {
+    let currNode = this.head;
+    while (!(currNode.next == null)) {
+      currNode = currNode.next;
+    }
+    return currNode;
+  }
+
+  dispReverse() {
+    let currNode = this.head;
+    currNode = this.findLast();
+    while (!(currNode.previous == null)) {
+      console.log(currNode.element);
+      currNode = currNode.previous;
+    }
+  }
 }
+
+var cities = new DList();
+cities.insert("Conway", "head");
+cities.insert("Russellville", "Conway");
+cities.insert("Carlisle", "Russellville");
+cities.insert("Alma", "Carlisle");
+cities.display();
+console.log();
+cities.remove("Carlisle");
+cities.display();
+console.log();
+cities.dispReverse();
