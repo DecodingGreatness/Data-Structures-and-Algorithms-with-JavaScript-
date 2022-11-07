660f1b7a3a270b6c028016614ff806e5742c4e98
Edward Ho <pythoned77@gmail.com>
2022-11-07 20:50:20 +0800

    added inOrder function. However function does not display value

--- BST/BST.js
+++ BST/BST.js
@@ -40,3 +40,22 @@ class BST {
     }
   }
 }
+
+const inOrder = (node) => {
+  if (!(node == null)) {
+    inOrder(node.left);
+    console.log(node.show() + " ");
+    inOrder(node.right);
+  }
+};
+
+let nums = new BST();
+nums.insert(23);
+nums.insert(45);
+nums.insert(16);
+nums.insert(37);
+nums.insert(3);
+nums.insert(99);
+nums.insert(22);
+console.log("Inorder traversal: ");
+inOrder(nums.root);
